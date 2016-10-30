2ddd562292b42a9ee872d40bdb72710fe4b3dd58
Sami Dakhani <samidakhani@gmail.com>
2016-10-30 19:58:19 -0400

    First commit

---
+++ src/main/java/org/dakhani/learn/Learn.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) GCOM Software Inc, NY. All Rights Reserved.
+ * 
+ * THIS INFORMATION IS COMPANY CONFIDENTIAL.
+ * 
+ * NOTICE: This material is a confidential trade secret and proprietary
+ * information of GCOM Software Inc, NY which may not be reproduced, used, sold, or
+ * transferred to any third party without the prior written consent of GCOM Software Inc, NY.
+ */
+package org.dakhani.learn;
+
+/**
+ * @author Sami Dakhani Created on Oct 30, 2016
+ *
+ */
+public class Learn {
+
+	public static void main(final String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
