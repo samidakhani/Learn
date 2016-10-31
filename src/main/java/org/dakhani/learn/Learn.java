e95586515c2c0f3210bda76e5a7722594012c984
Sami Dakhani <samidakhani@gmail.com>
2016-10-30 21:16:29 -0400

    Third commit

--- src/main/java/org/dakhani/learn/Learn.java
+++ src/main/java/org/dakhani/learn/Learn.java
@@ -16,7 +16,7 @@ package org.dakhani.learn;
 public class Learn {
 
 	public static void main(final String[] args) {
-		System.out.println("Hello World 1");
+		System.out.println("Hello World 2");
 	}
 
 }
