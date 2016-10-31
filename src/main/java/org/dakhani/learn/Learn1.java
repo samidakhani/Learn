1f6ddb0427938aef3991ba988a314bc4ea459c60
Sami Dakhani <samidakhani@gmail.com>
2016-10-31 18:48:16 -0400

    RTY

--- src/main/java/org/dakhani/learn/Learn1.java
+++ src/main/java/org/dakhani/learn/Learn1.java
@@ -10,13 +10,9 @@
 package org.dakhani.learn;
 
 /**
- * @author Sami Dakhani Created on Oct 30, 2016
+ * @author Sami Dakhani Created on Oct 31, 2016
  *
  */
-public class Learn {
-
-	public static void main(final String[] args) {
-		System.out.println("Dummy 3 acfzx afds asdas");
-	}
+public class Learn1 {
 
 }
