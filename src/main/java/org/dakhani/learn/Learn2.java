d367446cf7763774a6251c5c9674ed88a2650d32
Sami Dakhani <samidakhani@gmail.com>
2016-11-04 15:58:14 -0400

    Adding commit from develop

---
+++ src/main/java/org/dakhani/learn/Learn2.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) GCOM Software Inc, NY. All Rights Reserved.
+ * 
+ * THIS INFORMATION IS COMPANY CONFIDENTIAL.
+ * 
+ * NOTICE: This material is a confidential trade secret and proprietary
+ * information of GCOM Software Inc, NY which may not be reproduced, used, sold, or
+ * transferred to any third party without the prior written consent of GCOM Software Inc, NY.
+ */
+package org.dakhani.learn;
+
+/**
+ * @author Sami Dakhani Created on Nov 4, 2016
+ *
+ */
+public class Learn2 {
+
+	public static void main(final String[] args) {
+		System.out.println("Hello 2 Second");
+	}
+}
