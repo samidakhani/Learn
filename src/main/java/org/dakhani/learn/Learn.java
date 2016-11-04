a634afc911a437273091d9443f275d56a2ae7434
Sami Dakhani <samidakhani@gmail.com>
2016-11-04 19:03:43 -0400

    Comint56

--- src/main/java/org/dakhani/learn/Learn.java
+++ src/main/java/org/dakhani/learn/Learn.java
@@ -17,7 +17,7 @@ public class Learn {
 
 	public static void main(final String[] args) {
 		System.out.println("Adding a line 1 to develop");
-		System.out.println("Manages a new line");
+		System.out.println("Manages a new line 3");
 		System.out.println("Adding a line 2 to develop");
 	}
 
